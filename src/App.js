f1cf7c2383ada850c0ccaa3fb7f4f277c691512c
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-22 23:38:16 +0530

    added firebase config and homescreen component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <h1>netflix</h1>
+      <HomeScreen />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCePvJDHtrghX1WZRyfoP1NpQC2fSb-ffI",
+  authDomain: "netflix-clone-922c9.firebaseapp.com",
+  projectId: "netflix-clone-922c9",
+  storageBucket: "netflix-clone-922c9.appspot.com",
+  messagingSenderId: "913154081246",
+  appId: "1:913154081246:web:5b38509f763066ff33dd4f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
