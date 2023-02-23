1f4625cca8b011b9dd2694eeebf2768526dbe56c
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-24 00:25:40 +0530

    added some images

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import HomeScreen from "./HomeScreen";
 function App() {
   return (
     <div className="App">
-      <h1>netflix</h1>
       <HomeScreen />
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Nav.css";
 const Nav = () => {
-  return <div className="nav">Nav</div>;
+  return (
+    <div className="nav nav__black">
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          //   src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
