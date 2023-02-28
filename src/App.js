396e69a59ad7d084011e3d3c7f2576969cae9c16
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-28 23:17:38 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Banner from "./Banner";
+import Banner from "../Banner";
 import "./HomeScreen.css";
-import Nav from "./Nav";
-import requests from "./Request";
-import Row from "./Row";
+import Nav from "../Nav";
+import requests from "../Request";
+import Row from "../Row";
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
