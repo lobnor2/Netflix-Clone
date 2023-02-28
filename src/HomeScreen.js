789fc3605e3dba16d4ab4c5ae6cfa27d0e659fd6
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-28 22:46:47 +0530

    row component done with all the styles

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Request";
+import Row from "./Row";
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
@@ -9,7 +11,19 @@ const HomeScreen = () => {
 
       <Banner />
 
-      {/* row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
