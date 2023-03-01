97d3b634358c1d0c7c9cb34382c6fb813f4b2fac
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-03-01 23:52:32 +0530

    added signinscreen component and styles

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignInScreen from "./SignInScreen";
 const LoginScreen = () => {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className="loginScreen">
       <div className="loginScreen__background">
@@ -9,24 +12,37 @@ const LoginScreen = () => {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button className="loginScreen__button">Sign In</button>
+        <button className="loginScreen__button" onClick={() => setSignIn(true)}>
+          Sign In
+        </button>
         <div className="loginScreen__gradient" />
       </div>
       <div className="loginScreen__body">
-        <>
-          <h1>Unlimited films, TV programmes and more.</h1>
-          <h2>Watch anywhere, Cancel at any time.</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart you
-            membership.
-          </h3>
-          <div className="loginScreen__input">
-            <form>
-              <input type="email" placeholder="Email address" />
-              <button className="loginScreen__getStarted">GET STARTED</button>
-            </form>
-          </div>
-        </>
+        {signIn ? (
+          <>
+            <SignInScreen />
+          </>
+        ) : (
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere, Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart you
+              membership.
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen__getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SignInScreen.css";
+
+const SignInScreen = () => {
+  return <div>SignInScreen</div>;
+};
+
+export default SignInScreen;
