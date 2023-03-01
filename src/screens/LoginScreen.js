3ee2eba8bfe9aa726ae61b1b2d58942dab993888
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-03-01 23:44:49 +0530

    added loginscreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -15,6 +15,17 @@ const LoginScreen = () => {
       <div className="loginScreen__body">
         <>
           <h1>Unlimited films, TV programmes and more.</h1>
+          <h2>Watch anywhere, Cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart you
+            membership.
+          </h3>
+          <div className="loginScreen__input">
+            <form>
+              <input type="email" placeholder="Email address" />
+              <button className="loginScreen__getStarted">GET STARTED</button>
+            </form>
+          </div>
         </>
       </div>
     </div>
