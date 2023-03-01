4d96559fc1fa26503f2cb292059a791f9bab3a52
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-03-02 00:24:31 +0530

    done signinscreen with styles

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./SignInScreen.css";
 
 const SignInScreen = () => {
-  return <div>SignInScreen</div>;
+  return (
+    <div className="signInScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign In</button>
+        <h4>
+          New to Netflix? <span> Sign Up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 };
 
 export default SignInScreen;
