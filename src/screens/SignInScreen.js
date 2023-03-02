1874e56e05aa161f2d336227ebce20edd3898716
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-03-02 07:01:52 +0530

    done signinscreen styles, now working on firebase

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,15 +2,28 @@ import React from "react";
 import "./SignInScreen.css";
 
 const SignInScreen = () => {
+  const register = (e) => {
+    e.preventDefault();
+  };
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signInScreen">
       <form>
         <h1>Sign In</h1>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
         <h4>
-          New to Netflix? <span> Sign Up now.</span>
+          New to Netflix?{" "}
+          <span className="signUpScreen__link" onClick={register}>
+            {" "}
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
